b0f420739e90040a70c4519df67eb40dbba301ea
Ben Friedland <ben174@gmail.com>
2016-01-25 09:57:12 -0800

    Coin solution

--- coin.py
+++ coin.py
@@ -1,11 +1,23 @@
 
 
 def dem(amount, dems):
-    print amount
-    print dems
-    dems = set(sorted(dems))
-    for dem in dems:
-        print dem
+    if amount == 0:
+        return 1
+    elif amount < 0:
+        return 0
+    if not dems:
+        return 0
+    current_coin, rest_of_coins = dems[0], dems[1:]
 
 
-assert len(dem(4, [1, 2, 3])) == 4
+    num_possibilities = 0
+    while amount >= 0:
+        num_possibilities += dem(amount, rest_of_coins)
+        amount -= current_coin
+    return num_possibilities
+
+
+
+
+
+assert dem(4, [1, 2, 3]) == 4
