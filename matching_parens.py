c73d0bc9da70317e683a4e99575f8c7993e0cfbe
Ben Friedland <ben174@gmail.com>
2016-01-25 11:16:12 -0800

    Validate parens

--- matching_parens.py
+++ matching_parens.py
@@ -13,5 +13,24 @@ def matching_parens(sentence, pindex):
                 print index
                 return index
 
+
+
 sentence = "Sometimes (when I nest them (my parentheticals) too much (like this (and this))) they get confusing."
 assert matching_parens(sentence, 10) == 79
+
+def validate_parens(sentence):
+    openers = ['{', '[', '(']
+    closers = ['}', ']', ')']
+    stack = []
+    for index, char in enumerate(sentence):
+        if char in openers:
+            stack.append(char)
+        elif char in closers:
+            if not stack.pop() == openers[closers.index(char)]:
+                return False
+    return True
+
+
+assert validate_parens("{ [ ] ( ) }")
+assert not validate_parens("{ [ ( ] ) }")
+assert not validate_parens("{ [ }")
